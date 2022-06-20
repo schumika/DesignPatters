6ed514bc9a132f31030e14c5b7cb6f645954ec55
Anca Calugar <softacalugar@alarm.com>
2022-06-20 15:50:56 +0300

    Initial Commit

---
+++ SOLIDExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLIDExample
+//
+//  Created by Calugar Anca Maria on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
